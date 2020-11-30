68868681f74045f7d9ab5e67b577312faf0d4545
sandrine <liaworkingonline@gmail.com>
2020-11-30 13:09:12 +0100

    Création du composant TextButton

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -2,14 +2,12 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import TextButton from '../microComponents/TextButton';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <TextButton />
   </div>
 );
 
