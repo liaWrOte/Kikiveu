43b995bdad82aed2a9205fd5b054862e1c6d03c9
sandrine <liaworkingonline@gmail.com>
2020-12-01 09:55:55 +0100

    affichage de la map grace à une position absolute sur map et un z-index négatif

--- front/src/components/microComponents/Map/index.js
+++ front/src/components/microComponents/Map/index.js
@@ -9,17 +9,19 @@ import {
 import './index.scss';
 
 const Map = () => (
-  <MapContainer className="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+  <div className="map">
+    <MapContainer className="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  </div>
 );
 
 export default Map;
