629f30a7a1e438f945d4dfa844355c66173cbab7
Kevin <bouris.kevin@gmail.com>
2020-12-01 15:48:56 +0100

    installation du bundle lexik_jwt_ authentication pour les authentification a l'API a voir plus tard

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
