494e5e3354d23442ff9d1fe2023b4127f8067699
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-12-01 16:11:36 +0100

    ajout method browse/read/edit dans UserController et ajout de UserEditType Form

--- back/src/Controller/Api/UserController.php
+++ back/src/Controller/Api/UserController.php
@@ -3,13 +3,15 @@
 namespace App\Controller\Api;
 
 use App\Entity\Users;
+use App\Form\UserEditType;
 use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/api/v1/user", name="api_v1_user_")
+ * @Route("/api/v1/user", name="api_v1_")
  */
 class UserController extends AbstractController
 {
@@ -29,7 +31,46 @@ class UserController extends AbstractController
      */
     public function read(UsersRepository $usersRepository, $id): Response
     {
-        dd($user = $usersRepository->findById($id));
+        $user = $usersRepository->findById($id);
         return dd($this->json($user));
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit", methods={"PUT"})
+     */
+    public function edit(Request $request): Response
+    {
+        $json = $request->getContent();
+        $userArray = json_decode($json, true);
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserEditType::class, $user, ['csrf_protection' => false]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->json($user, 201);
+        }
+    }
+
+    /**
+     * @Route("/edit/{id}", name="localisation_edit", methods={"PATCH"})
+     */
+    public function editLocalisation()
+    {
+
+    }
+
+    /**
+     * @Route("/delete/{id}", name="DELETE", methods={"DELETE"})
+     */
+    public function delete()
+    {
+        $json = $request->getContent();
     }
 }
---
+++ back/src/Form/UserEditType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Email,
+                    new NotBlank,
+                ],
+            ])
+            ->add('firstname', null, [
+                'constraints' => new NotBlank,
+            ])
+            ->add('lastname', null, [
+                'constraints' => new NotBlank,
+            ])
+            ->add('pseudo', null, [
+                'constraints' => new NotBlank,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
