fecac704d2313b6429dc429f456e2f496c1db1a8
Kevin <bouris.kevin@gmail.com>
2020-12-03 17:42:41 +0100

    ajout des contraintes de validation sur le form usersType

--- back/src/Form/UsersType.php
+++ back/src/Form/UsersType.php
@@ -4,20 +4,33 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UsersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // TODO Contrainte Validation
+        // TODO Voir pour le slug et le password 
         $builder
-            ->add('email')
-            ->add('pseudo')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Email(),
+                    new NotBlank(),
+                ],
+            ])
+            ->add('pseudo', null, [
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ])
             ->add('slug')
-            ->add('password')
+            ->add('password', PasswordType::class)
         ;
     }
 
