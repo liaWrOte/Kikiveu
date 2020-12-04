b6231b1d2d9de6245240512783822c0c1d69e4ba
Jordane-Kraken <jordane.minaya@gmail.com>
2020-12-04 15:14:14 +0100

    production du front OK fonctionnelle, suppression des fichiers inutiles à la racine de kikiveu, middleware auth toujours en cours

--- front/src/middlewares/auth.js
+++ front/src/middlewares/auth.js
@@ -23,7 +23,9 @@ const auth = (store) => (next) => (action) => {
         // traitement si réponse est un succès
           console.log('middleware : login');
           console.log(response);
-          // axios.defaults.headers.common = { Authorization: `Bearer ${response.data.token}` }
+          // on envoie les nouvelles infos dans notre state
+          store.dispatch(saveAuthInfo(response.data.logged, response.data.pseudo));
+          window.localStorage.accessToken = response.data.token;
           const config = {
             headers: { Authorization: `Bearer ${response.data.token}` },
           };
@@ -36,16 +38,15 @@ const auth = (store) => (next) => (action) => {
               console.log(error);
             })
             .then((tokenResponse) => {
-              console.log('token');
               console.log(tokenResponse);
             })
             .catch((error) => {
               console.log(error);
             });
         });
-
       next(action);
       break;
+  
 
     default:
       // on passe l'action au suivant (middleware suivant ou reducer)
