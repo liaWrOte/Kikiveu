26f71b90623c5e9b1e62eb4a30ead51bf4691c6c
sandrine <liaworkingonline@gmail.com>
2020-12-13 10:11:47 +0100

    suppresion des messages du bottom, remplacé par l'aide

--- front/src/components/microComponents/Header/index.js
+++ front/src/components/microComponents/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import { HelpCircle, Power } from 'react-feather';
+import { Power } from 'react-feather';
 import logo from '../../../assets/images/logo.png';
 
 import './index.scss';
@@ -10,7 +10,6 @@ import './index.scss';
 const Header = ({ handleLogout }) => (
   <header className="header">
     <div>
-      <Link to="/contact"><HelpCircle className="header__icone" stroke-width={2} width={27} height={27} /></Link>
     </div>
     <div className="header__item">
       <Link to="/"><img src={logo} alt="logo KikiVeu" className="header__item__img" /></Link>
