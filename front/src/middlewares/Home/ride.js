32ab530101d37af67cf4a69732160acd57b37d11
sandrine <liaworkingonline@gmail.com>
2020-12-15 12:50:20 +0100

    mise en place de l'appel axios pour les avatar mais pour le moment nous n'en avons pas en bdd, à tester plus tard

--- front/src/middlewares/Home/ride.js
+++ front/src/middlewares/Home/ride.js
@@ -41,7 +41,7 @@ const rideMiddleware = (store) => (next) => (action) => {
 
     case HANDLE_POST_COMMENT:
       // je récupère les données qui m'intéressent : email et mdp
-      
+
       axios.post('http://localhost:8000/api/v1/comment/add', {
         users: auth.userId,
         body: ride.comment,
@@ -58,7 +58,7 @@ const rideMiddleware = (store) => (next) => (action) => {
         .catch((error) => {
           console.log(error);
         });
-      
+
       next(action);
       break;
 
@@ -72,9 +72,19 @@ const rideMiddleware = (store) => (next) => (action) => {
           store.dispatch(saveComments(response.data));
           console.log(response);
           // console.log(localStorage.getItem('token'));
+          axios.get(`http://localhost:8000/api/v1/dog/${ride.rideInfos.userId}`,
+            config)
+            .then((response2) => {
+            // traitement si réponse est un succès
+              console.log('middleware : get all avatar');
+              console.log(response2);
+            })
+            .catch((error2) => {
+              console.log(error2);
+            });
         })
-        .catch((error) => {
-          console.log(error);
+        .catch((error1) => {
+          console.log(error1);
         });
 
       next(action);
