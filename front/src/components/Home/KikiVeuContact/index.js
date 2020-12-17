6c1325c053a48cad0cec7434e57498a1eae9239c
Jordane-Kraken <jordane.minaya@gmail.com>
2020-12-17 20:55:08 +0100

    ajout de l'email dans contact KikiVeu

--- front/src/components/Home/KikiVeuContact/index.js
+++ front/src/components/Home/KikiVeuContact/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Mail } from 'react-feather';
 
 // Import composants
 import PreviousButton from '../../microComponents/PreviousButton';
@@ -9,28 +10,14 @@ import TextButton from '../../microComponents/TextButton';
 
 import './KikiVeuContact.scss';
 
-const KikiVeuContact = () => {
-  const cguText = 'Accéder aux CGU';
-  const sendText ='Envoyer';
-  return (
+const KikiVeuContact = () => (
     <div className="kikiVeuContact">
       <div className="kikiVeuContact__wrapper">
-        <h2 className="kikiVeuContact__item">Formulaire de contact KikiVeu</h2>
-        <Link to="/cgu"><TextButton text={cguText} /></Link>
-        <select className="kikiVeuContact__item">
-          <option value="info">Demander un renseignement</option>
-          <option value="signal">Signaler un utilisateur</option>
-          <option value="signal">Supprimer mon compte KikiVeu</option>
-        </select>
-        <div className="kikiVeuContact__item">
-          <label htmlFor="comment">Commentaire libre</label>
-          <textarea id="comment" name="comment" rows="10" cols="44" />
-        </div>
-        <PreviousButton />
-        <TextButton text={sendText} />
+        <h2 className="kikiVeuContact__item"><Mail />Pour contacter la team KikiVeu :</h2>
+        <p className="kikiVeuContact__item">kikiveu.contact@gmail.com</p>
       </div>
+      <PreviousButton />
     </div>
   );
-};
 
 export default KikiVeuContact;
